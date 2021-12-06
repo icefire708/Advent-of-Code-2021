4cb63f84552f9990c6b81194dbe7cea90ccf410a
Sergey <nikiforov708@gmail.com>
2021-12-06 08:19:32 +0300

    day 6

---
+++ 6_1.py
@@ -0,0 +1,13 @@
+from typing import Counter
+
+
+with open('6.txt', 'r') as f:
+    laternfishes = Counter(map(int, f.read().split(',')))
+    for _ in range(80):
+        new_laternfishes = Counter()
+        for i in range(8):
+            new_laternfishes[i] = laternfishes[i + 1]
+        new_laternfishes[6] += laternfishes[0]
+        new_laternfishes[8] = laternfishes[0]
+        laternfishes = new_laternfishes
+print(sum(laternfishes.values()))
