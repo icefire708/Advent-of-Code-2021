c8d47aba0d2fccd8df7d4462eaf3c57a6467eda1
BuildTools <unconfigured@null.spigotmc.org>
2021-12-13 08:36:47 +0300

    day 13

---
+++ 13_2.py
@@ -0,0 +1,55 @@
+dots = []
+folds = []
+size = 0
+with open('13.txt', 'r') as f:
+    sss = f.readlines()
+    for i, line in enumerate(sss):
+        line = line.strip()
+        if line:
+            x, y = map(int, line.split(','))
+            dots.append((x, y))
+            size = max(size, x, y)
+        else:
+            break
+    for i in range(i + 1, len(sss)):
+        line = sss[i]
+        left, value = line.split('=')
+        folds.append((left[-1], int(value)))
+
+size += 1
+mat = [['.' for _ in range(size)] for _ in range(size)]
+for x, y in dots:
+    mat[y][x] = '#'
+heigh = width = size
+
+
+def hor(y_line):
+    for i in range(y_line):
+        for x in range(width):
+            if mat[y_line * 2 - i][x] == '#':
+                mat[i][x] = '#'
+
+def vert(x_line):
+    for y in range(heigh):
+        for x in range(x_line):
+            if mat[y][x_line * 2 - x] == '#':
+                mat[y][x] = '#'
+
+for fold_type, value in folds:
+    if fold_type == 'y':
+        hor(value)
+        heigh = value
+    else:
+        vert(value)
+        width = value
+
+ans = 0
+for i in range(heigh):
+    for j in range(width):
+        if mat[i][j] == '#':
+            ans += 1
+
+for y in range(heigh):
+    for x in range(width):
+        print(mat[y][x], end='')
+    print()
